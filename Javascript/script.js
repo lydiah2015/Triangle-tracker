586bc321b531e0417c50027afac851ab95a7eafc
Elle <mitchellemakini15@gmail.com>
2019-02-16 15:10:53 +0300

    add changes made to Javascript/script.js

---
+++ Javascript/script.js
@@ -0,0 +1,26 @@
+function triangle() {
+  var sideA = parseInt(document.getElementById('a').value);
+  console.log(sideA);
+  var sideB = parseInt(document.getElementById('b').value);
+  console.log(sideB);
+  var sideC = parseInt(document.getElementById('c').value);
+  console.log(sideC);
+
+  if(((sideA+sideB !=sideC) || (sideA+sideC!=sideB) || (sideB+sideC!=sideA)) && ((sideA+sideB<sideC) || (sideA+sideC<sideB) || (sideB+sideC<sideA))){
+      console.log("so far so good");
+      document.getElementById("result").innerHTML = "Not a triangle";
+  }
+  else if (sideA != sideB && sideB != sideC && sideA != sideC)  {
+    alert("Working")
+  }
+  else if(sideA + sideB > sideC || sideB + sideC > sideA || sideA + sideC > sideB){
+    alert("Still working")
+  }
+   else if (sideA === sideB && sideB === sideC && sideC === sideA){
+    alert("equilateral triangle");
+  }else if (sideA === sideB || sideB === sideC ||sideC === sideA) {
+    alert("isoslece triangle");
+  }else{
+    alert("not triangle");
+  }
+}
