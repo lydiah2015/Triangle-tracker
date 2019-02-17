e5fd7c360c751fee17483a80b8ab772b759c9956
Elle <mitchellemakini15@gmail.com>
2019-02-17 20:44:56 +0300

    initial changes

--- Javascript/script.js
+++ Javascript/script.js
@@ -13,7 +13,7 @@ function triangle() {
   }
   else if (sideA != sideB && sideB != sideC && sideA != sideC) {
     // console.log("working")
-    document.getElementById("result").innerHTML = 'Scalene triangle <img src="/home/user/Desktop/Triangle-tracker/images/Screenshot from 2019-02-17 20-26-33.png">'';
+    document.getElementById("result").innerHTML = 'Scalene triangle <img src="images/scalene.png">';
   }
   else if (sideA === sideB && sideB === sideC && sideA === sideC) {
     // console.log("we are still working")
