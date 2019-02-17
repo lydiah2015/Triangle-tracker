dab364aed0a38e3399e7376dc3d22fd7ebc27837
Elle <mitchellemakini15@gmail.com>
2019-02-17 14:33:51 +0300

    add all changes made ro Javascript

---
+++ Javascript/script.js
@@ -0,0 +1,29 @@
+function triangle() {
+  var sideA = parseInt(document.getElementById('a').value);
+  // console.log(sideA);
+  var sideB = parseInt(document.getElementById('b').value);
+  // console.log(sideB);
+  var sideC = parseInt(document.getElementById('c').value);
+  // console.log(sideC);
+
+  if(((sideA+sideB !=sideC) || (sideA+sideC!=sideB) || (sideB+sideC!=sideA)) && ((sideA+sideB<sideC) || (sideA+sideC<sideB) || (sideB+sideC<sideA))){
+      // console.log("so far so good");
+      document.getElementById("result").innerHTML = "Not a triangle";
+  }
+  else if (sideA != sideB && sideB != sideC && sideA != sideC) {
+    // console.log("working")
+    document.getElementById("result").innerHTML = "Scalene triangle";
+  }
+  else if (sideA === sideB && sideB === sideC && sideA === sideC) {
+    // console.log("we are still working")
+document.getElementById("result").innerHTML = "Equilateral triangle";
+  }
+  else if ((sideA === sideB != sideC) || (sideB === sideC != sideA) || (sideA === sideC != sideB)) {
+    // console.log("ts there")
+    document.getElementById("result").innerHTML = "Isosceles triangle"
+  }
+  else {
+    // console.log("we are done")
+    document.getElementById("result").innerHTML = "Not a triangle"
+  }
+}
