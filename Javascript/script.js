f53fd9fe88d64b71029a3f5f208d86603066f6f3
Elle <mitchellemakini15@gmail.com>
2019-02-18 17:54:36 +0300

    add not a triangle property

--- Javascript/script.js
+++ Javascript/script.js
@@ -5,7 +5,6 @@ function triangle() {
   // console.log(sideB);
   var sideC = parseInt(document.getElementById('c').value);
   // console.log(sideC);
-<<<<<<< HEAD
   var triangles = ["Equilateral triangle", "Isosceles triangle", "Scalene triangle"];
 
   // if(((sideA+sideB !=sideC) || (sideA+sideC!=sideB) || (sideB+sideC!=sideA)) && ((sideA+sideB<sideC) || (sideA+sideC<sideB) || (sideB+sideC<sideA))){
@@ -19,35 +18,13 @@ function triangle() {
     } else if ((sideA == sideB) || (sideB == sideC) || (sideA == sideC)) {
       document.getElementById("result").innerHTML = 'Isosceles triangle <img src = "/home/user/Desktop/Triangle-tracker/images/isosceles.png">'
     } else {
-      document.getElementById("result").innerHTML = "Scalene triangle"
+      document.getElementById("result").innerHTML = 'Scalene triangle <img src = /home/user/Desktop/Triangle-tracker/images/scalene.png>'
     }
   } else {
     document.getElementById("result").innerHTML = "Not a triangle"
   }
+}
 
   function reload() {
     location.reload();
-=======
-
-  if(((sideA+sideB !=sideC) || (sideA+sideC!=sideB) || (sideB+sideC!=sideA)) && ((sideA+sideB<sideC) || (sideA+sideC<sideB) || (sideB+sideC<sideA))){
-      // console.log("so far so good");
-      document.getElementById("result").innerHTML = "Not a triangle";
   }
-  else if (sideA != sideB && sideB != sideC && sideA != sideC) {
-    // console.log("working")
-    document.getElementById("result").innerHTML = "Scalene triangle";
-  }
-  else if (sideA === sideB && sideB === sideC && sideA === sideC) {
-    // console.log("we are still working")
-document.getElementById("result").innerHTML = "Equilateral triangle";
-  }
-  else if ((sideA === sideB != sideC) || (sideB === sideC != sideA) || (sideA === sideC != sideB)) {
-    // console.log("ts there")
-    document.getElementById("result").innerHTML = "Isosceles triangle"
-  }
-  else {
-    // console.log("we are done")
-    document.getElementById("result").innerHTML = "Not a triangle"
->>>>>>> 7cc5ec12020b70f25fda8f16d1b67e9a8e5eb08b
-  }
-}
